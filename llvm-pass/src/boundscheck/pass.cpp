2a65a66701a5c22236f9413002e0806063d0bf1f
Pim Pieters <Pim.Pieters@UGent.be>
2024-04-23 15:51:54 +0200

    Clean up static case

--- llvm-pass/src/boundscheck/pass.cpp
+++ llvm-pass/src/boundscheck/pass.cpp
@@ -59,22 +59,61 @@ public:
       // ASSIGNMENT: Implement your pass here.
       // Error message template for static case:
 
-      // Get index
-      int index = -1;
-      if (auto CI = llvm::dyn_cast<llvm::ConstantInt>(GEP->getOperand(2)))
-        index = CI->getZExtValue();
-      
+      // Debug info
+      auto debugLoc = GEP->getDebugLoc();
+      auto fileName = debugLoc->getFilename().str();
+      auto fileLine = debugLoc->getLine();
+
       // Get array size
       auto array_pointer = GEP->getOperand(0); // Get the pointer to the array
       auto array_type = dyn_cast<PointerType>(array_pointer->getType())->getPointerElementType(); // Get the underlying type of the array (e.g. [10 x i64])
       auto array_size = array_type->getArrayNumElements(); // Get the size of the array
 
-      // Verify location
-      if (index < 0 || index >= array_size) {
-        auto debugLoc = GEP->getDebugLoc();
-       auto message = fmt::format("out-of-bounds array access detected at {}:{}", debugLoc->getFilename().str(), debugLoc->getLine());
-       LLVM_DEBUG(dbgs() << "ERROR: " << message.c_str() << "\n"); 
+      // Get index
+      auto index_operand = GEP->getOperand(2);
+      // Check if index is constant value
+      if (auto CI = llvm::dyn_cast<llvm::ConstantInt>(index_operand)) {
+        int index = CI->getZExtValue();
+        // Verify location
+        if (index < 0 || index >= array_size) {
+          auto message = fmt::format("out-of-bounds array access detected at {}:{}", fileName, fileLine);
+          llvm::report_fatal_error(StringRef(message), false);
+        }
       }
+      // Otherwise it's dynamic
+      else {
+        LLVM_DEBUG(dbgs() << "Dynamic index found\n");
+        /*WORKING ON THIS -Pim
+        int id = 1;
+        auto funcName = GEP->getFunction()->getName().str();
+        LLVM_DEBUG(dbgs() << funcName << " Hey :)\n");
+        LLVM_DEBUG(dbgs() << "a\n");
+
+        std::string if_name = fmt::format("if_block_{}_{}", funcName, id);
+        std::string else_name = fmt::format("else_block_{}_{}", funcName, id);
+        std::string end_name = fmt::format("end_block_{}_{}", funcName, id);
+
+        llvm::BasicBlock *if_block = llvm::BasicBlock::Create(F.getContext(), if_name, &F);
+        llvm::BasicBlock *else_block = llvm::BasicBlock::Create(F.getContext(), else_name, &F);
+        llvm::BasicBlock *end_block = llvm::BasicBlock::Create(F.getContext(), end_name, &F);
+
+        Value* condition = Builder.getInt1(1);
+
+        Instruction* instr = Builder.CreateCondBr(condition, if_block, else_block);
+
+        Builder.SetInsertPoint(if_block);
+        Builder.CreateBr(end_block);
+        Builder.SetInsertPoint(else_block);
+        Builder.CreateBr(end_block);
+        Builder.SetInsertPoint(end_block);
+
+        LLVM_DEBUG(dbgs() << "e\n");
+        LLVM_DEBUG(dbgs() << instr << "\n");
+        GEP->insertBefore(instr);
+        LLVM_DEBUG(dbgs() << "f\n");
+        */
+      }
+
 
       // Error message template for dynamic case:
 
